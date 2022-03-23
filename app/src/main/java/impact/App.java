8fd971c36eaef1f7c8beb162276812bc90871c65
masai <mahapamasai@gmail.com>
2022-03-23 07:44:59 +0200

    added .gitignore

--- app/src/main/java/impact/App.java
+++ app/src/main/java/impact/App.java
@@ -3,8 +3,7 @@ package impact;
 public class App {
 
     public static void main(String[] args) {
-//        int[] numbers = {1,2,3,5,7,8,9,12,13,15,16, 18};
-        int[] numbers = {null};
+        int[] numbers = {1,2,3,5,7,8,9,12,13,15,16, 18};
         System.out.println(RangeBuilder.createRanges(numbers));
     }
 }
